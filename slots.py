d9debaa32cb035fac130dc9ec59ac8eb997c7617
Caseirooo <franciscocfrc@gmail.com>
2025-10-08 17:04:25 +0100

    Update slots.py

--- slots.py
+++ slots.py
@@ -1,16 +1,32 @@
 import random
 
+
+def deposit():
+    while True:
+        amount = input("How much do you want to deposit?\n$")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                balance += amount
+                with open("balance.txt", "w") as f:
+                    f.write(str(balance))
+                break
+            else:
+                print("Amount has to be greater than 0.")
+        else:
+            print("Amount has to be a number.")
+
+
 with open("balance.txt", "r") as f:
     balance = int(f.read())
 while True:
-    choice = int(input("\nWhat do you want to do?\n1. Deposit\n2. Check Balance\n3. GOLD GOLD GOLD\n4. Quit\n"))
+    choice = int(input(
+        "\nWhat do you want to do?\n1. Deposit\n2. Check Balance\n3. GOLD GOLD GOLD\n4. Quit\n"))
     if choice == 1:
-        amount = int(input("How much do you want to deposit\n"))
-        balance += amount
-        with open("balance.txt", "w") as f:
-            f.write(str(balance))
+        deposit()
     elif choice == 2:
         print(balance)
     elif choice == 3:
+        break
     elif choice == 4:
         break
